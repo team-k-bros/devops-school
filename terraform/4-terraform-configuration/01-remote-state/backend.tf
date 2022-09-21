39a257d885d5a0f191e9a71588569a910db8ebae
Marco Kwak <marcosdeseul@gmail.com>
2022-09-21 14:12:33 +0800

    rename remote-state folder and other tf files accordingly

--- terraform/4-terraform-configuration/01-remote-state/backend.tf
+++ terraform/4-terraform-configuration/01-remote-state/backend.tf
@@ -3,7 +3,7 @@
 #     profile        = "<update-aws-configure-profile>"
 #     bucket         = "terraform-s3-bucket-devops-school" # s3 bucket 이름
 #     key            = "terraform/terraform.tfstate" # s3 내에서 저장되는 경로
-#     region         = "ap-northeast-2"
+#     region         = "ap-southeast-1" # 원하는 AWS 리전
 #     encrypt        = true
 #     dynamodb_table = "terraform-lock"
 #   }
